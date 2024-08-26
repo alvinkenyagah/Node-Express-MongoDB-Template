e0c21fa2477ec7d03312df3b44a780e48fe8d7a9
alvinkenyagah <alvin47k@gmail.com>
2024-08-26 17:27:51 +0300

    add nodemon

--- index.js
+++ index.js
@@ -1,13 +1,8 @@
 const express = require('express')
 const app = express()
 
-// app.get('/', function (req, res) {
-//   res.send('Hello World')
-// })
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
 
-// app.listen(3000)
-
-app.listen(3000, ()=>{console.log("123")});
-
-
-app.get('/',(req, res)=>{res.send("hello node api")})
+app.listen(3000)
