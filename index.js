81e0ed0408f79cbe74ae2dab2016d68833d251a6
alvinkenyagah <alvin47k@gmail.com>
2024-08-28 13:53:14 +0300

    Complete template node express mongodb server

--- index.js
+++ index.js
@@ -1,8 +1,30 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+const Product = require('./models/products.model.js');
+const mongoose = require('mongoose');
+const productRoute = require('./routes/product.route.js');
+require('dotenv').config();  // Load environment variables
+
+app.use(express.json());
+
+// FORMURLENCODED SUPPORT
+app.use(express.urlencoded({ extended: false }));
+
+// ROUTES
+app.use("/api/products", productRoute);
 
 app.get('/', function (req, res) {
-  res.send('Hello World')
-})
+  res.send('Hello World');
+});
 
-app.listen(3000)
+
+const mongoURI = process.env.MONGODB_URI;
+
+mongoose.connect(mongoURI)
+  .then(() => {
+    console.log('Connected to database!');
+    app.listen(3000, () => {
+      console.log("Server running on port 3000");
+    });
+  })
+  .catch(() => console.log("Failed to connect to database!"));
---
+++ routes/product.route.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router()
+const Product = require('../models/products.model.js')
+const {getProducts, getProduct, createProduct, deleteProduct, updateProduct } = require('../controllers/product.controller.js')
+
+
+
+router.get('/', getProducts );
+router.get('/:id', getProduct );
+router.post('/', createProduct );
+router.delete('/:id', deleteProduct );
+router.put('/:id', updateProduct );
+
+
+
+module.exports = router
